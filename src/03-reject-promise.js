e234979252d180d152250e29298abb30e059e22d
iamBharatManral <justbharatmanral@gmail.com>
2023-07-24 16:22:14 +0530

    complete: reject promise

---
+++ src/03-reject-promise.js
@@ -0,0 +1,7 @@
+const promise = new Promise((resolve, reject) => {
+    setTimeout(() => {
+        reject(new Error('REJECTED!'))
+    }, 300)
+})
+
+promise.then(() => {}, err => console.log(err.message))
